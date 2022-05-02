cdb0f04139a3c4e1e422c2189efbce07d02a95c6
Ryohei Ikegami <iofg2100@gmail.com>
2022-05-02 16:50:23 +0900

    Add options type

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,14 @@
 type Point = [number, number];
 
-declare function subdivide(bezierPoints: [Point, Point, Point, Point]): Point[];
+interface Options {
+  approximationScale?: number;
+  angleTolerance?: number;
+  cuspLimit?: number;
+}
+
+declare function subdivide(
+  bezierPoints: [Point, Point, Point, Point],
+  options?: Options
+): Point[];
 
 export default subdivide;
